80894e8cd82b1144472df05abb51b005ca2924c9
Gaurav Kumar <84120770+Gauravkr1286@users.noreply.github.com>
2023-04-07 23:42:07 +0530

    Create console.js

---
+++ console.js
@@ -0,0 +1,16 @@
+//This is my console file//
+// program to write to console
+
+// passing number 
+console.log(8);
+
+// passing string
+console.log('hello');
+
+// passing variable
+const x = 'hello';
+console.log(x);
+
+// passing function
+function sayName() {
+    return 'Hello John';
